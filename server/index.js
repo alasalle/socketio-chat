d3470b43b8998c199299873333a41c5647c29131
alasalle <angela.lasalle91@gmail.com>
2020-01-13 20:29:16 -0500

    project init

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+
+const PORT = process.env.PORT || 5000;
+
+const router = require('./router');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on('connection', (socket) => {
+    console.log('we have a new connection')
+    socket.on('disconnect', () => {
+        console.log('user has left')
+    })
+})
+
+app.use(router);
+
+server.listen(PORT, err => {
+if (err) throw err
+console.log(`Server is listening on port ${PORT}`)
+})
